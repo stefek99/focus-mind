2482769ca7b230695d5564fbb09ef03d86493a9b
astralship genesis <mstefanow@gmail.com>
2018-09-08 13:04:00 +0100

    work work work

--- app.js
+++ app.js
@@ -3,20 +3,73 @@ var app = angular.module("app", ["ngRoute"]);
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/home', {
-      templateUrl: 'home.html',
+      templateUrl: 'partials/home.html',
       controller: "HomeCtrl"
     })
+    .when('/countdown', {
+      templateUrl: 'partials/countdown.html',
+      controller: "CountdownCtrl"
+    })
     .when('/settings', {
-      templateUrl: 'settings.html',
+      templateUrl: 'partials/settings.html',
       controller: "SettingsCtrl"
     })
     .otherwise('/home')
 });
 
-app.controller("HomeCtrl", function($scope) {
-  $scope.message = "It home!";
+app.controller("HomeCtrl", function($scope, countdown) {
+  $scope.minutes = 1;
+  $scope.seconds = 30;
+  $scope.accomplish = "required";
+
+  $scope.submit = function() {
+    var seconds = $scope.minutes * 60 + $scope.seconds;
+    countdown.start($scope.accomplish, seconds);
+    return false;
+  }
 });      
 
-app.controller("LoginCtrl", function($scope) {
+app.controller("CountdownCtrl", function($scope, $location, countdown) {
+  $scope.Math = window.Math;
+  $scope.accomplish = countdown.accomplish;
+  $scope.seconds = countdown.seconds;
+
+  if (!$scope.accomplish) { $location.path("/"); }  // checking if we are initialized
+
+  $scope.$on("second", function(event, data) {
+    $scope.seconds = data.seconds;
+    $scope.$apply();
+  })
+  
+});
+
+app.controller("SettingsCtrl", function($scope) {
   $scope.message = "It works!";
 });
+
+app.service("countdown", function($rootScope, $location) {
+  var countdown = {};
+
+
+
+  countdown.start = function(accomplish, seconds) {
+    countdown.accomplish = accomplish;
+    countdown.seconds = seconds;
+    countdown.intervalId = setInterval(function() {
+      countdown.seconds--;
+      console.log(countdown.seconds);
+
+      $rootScope.$broadcast("second", {seconds: countdown.seconds})
+
+      if (countdown.seconds === 0) {
+        clearInterval(countdown.intervalId);
+      }
+      
+    }, 1000);
+
+    $location.path("countdown");
+  }
+
+  return countdown;
+
+})
